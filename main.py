1b2b589b849de86b70396898b4e06ace25f2945b
TemTemPrime <ephraimplayz@gmail.com>
2026-03-11 10:44:53 +0100

    minor error fix

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from key import key
+
 import os
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
-from google import genai
 
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise Exception("api_key not found")
+st.set_page_config(page_title="PDF RAG Assistant", layout="wide")
+st.title("PDF Question Answering Assistant")
+uploaded_files = st.file_uploader("Upload PDF files", type=["pdf"], accept_multiple_files=True)
 
-client = genai.Client(api_key=key)
+
 model = ChatGoogleGenerativeAI(
     model = 'gemini-2.5-flash',
     temperature = 0,
