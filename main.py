e9028ec471df2ffbdb7f20d91bfe47ec2540941f
TemTemPrime <ephraimplayz@gmail.com>
2026-03-11 10:55:41 +0100

    api thingy

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import os
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-api_key = os.environ.get("GEMINI_API_KEY")
+api_key = st.secrets.get("GEMINI_API_KEY")
 if api_key == None:
-    raise Exception("api_key not found")
+    st.error("Gemini api key not found. please add it in steamlit secrets")
+    st.stop
 st.set_page_config(page_title="PDF RAG Assistant", layout="wide")
 st.title("PDF Question Answering Assistant")
 uploaded_files = st.file_uploader("Upload PDF files", type=["pdf"], accept_multiple_files=True)
